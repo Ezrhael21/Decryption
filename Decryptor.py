0c382849e0e28116a647dd1957b7dabb0bae7fd0
Ezrhael21 <ezrhaelsicat21@gmail.com>
2023-04-18 23:19:21 +0800

    Import colorama

--- Decryptor.py
+++ Decryptor.py
@@ -3,29 +3,30 @@
 # 04/17/2023
 # Program 2 - Decryption
 
+import colorama
 import pyfiglet
 
-print ("=" * 100)
+print (colorama.Fore.GREEN + "=" * 100)
 font = pyfiglet.figlet_format("Code Decryption", font = "big" )
-print ("=" * 100)
+print (colorama.Fore.BLUE + font)
 
 # Introduction to the program
-print ("=" * 100)
-Name=input("Enter your username: ")
-print ("=" * 100)
-print ("Hello!", Name,)
+print (colorama.Fore.GREEN + "=" * 100)
+Name = input(colorama.Fore.BLUE + "Enter your username: ")
+print (colorama.Fore.GREEN + "=" * 100)
+print (colorama.Fore.YELLOW + "Hello!", Name,)
 print ("Today, we are going to decrypt a code")
 
 moredata = True
 while moredata:
 
     # Ask the user for input
-    print ("=" * 100)
-    user_text = input("Input your message to decrypt: ")
+    print (colorama.Fore.GREEN + "=" * 100)
+    user_text = input(colorama.Fore.WHITE + "Input your message to decrypt: ")
 
     # Time Delay
-    print ("=" * 100)
-    print ("Processing...")
+    print (colorama.Fore.GREEN + "=" * 100)
+    print (colorama.Fore.WHITE + "Processing...")
     import time
     time.sleep(5)
     
@@ -33,13 +34,13 @@ while moredata:
     decrypted_string = user_text.replace("*", "a").replace("&", "e").replace("#", "i").replace("+", "o").replace("!", "u")
     
     # Print Output
-    print ("=" * 100)
-    print ("Original Message : ", user_text)
-    print ("Decrypted Message: ", decrypted_string)
-    print ("=" * 100)
+    print (colorama.Fore.GREEN + "=" * 100)
+    print (colorama.Fore.RED + "Original Message : ", colorama.Fore.WHITE + user_text)
+    print (colorama.Fore.RED + "Decrypted Message: ", colorama.Fore.WHITE + decrypted_string)
+    print (colorama.Fore.GREEN + "=" * 100)
 
     while True:
-        repeat = input("Do you want to try again? (Yes/No): ")
+        repeat = input(colorama.Fore.WHITE + "Do you want to try again? (Yes/No): ")
         if repeat.lower() == "yes":
             break
         elif repeat.lower() == "no":
@@ -48,9 +49,9 @@ while moredata:
         else:
             print("Invalid input. Please enter 'Yes' or 'No'.")
 
-print ("=" * 100)
-print ("Thank you for using this program.")
-print ("=" * 100)
+print (colorama.Fore.GREEN + "=" * 100)
+print (colorama.Fore.YELLOW + "Thank you for using this program.")
+print (colorama.Fore.GREEN + "=" * 100)
     
     
 
