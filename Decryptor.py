b624fa6b9325485d60a1c894fa94cec020f723a4
Ezrhael21 <ezrhaelsicat21@gmail.com>
2023-04-18 22:58:53 +0800

    Ask the user if he/she wants to try again

--- Decryptor.py
+++ Decryptor.py
@@ -3,12 +3,31 @@
 # 04/17/2023
 # Program 2 - Decryption
 
-# Ask the user for input
-user_text = input("Input your message to decrypt: ")
-# Replace the character to its corresponding value
-decrypted_string = user_text.replace("*", "a").replace("&", "e").replace("#", "i").replace("+", "o").replace("!", "u")
-# Print Output
-print (decrypted_string)
+moredata = True
+while moredata:
+
+    # Ask the user for input
+    user_text = input("Input your message to decrypt: ")
+    
+    # Replace the character to its corresponding value
+    decrypted_string = user_text.replace("*", "a").replace("&", "e").replace("#", "i").replace("+", "o").replace("!", "u")
+    
+    # Print Output
+    print (decrypted_string)
+
+    while True:
+        repeat = input("Do you want to try again? (Yes/No): ")
+        if repeat.lower() == "yes":
+            break
+        elif repeat.lower() == "no":
+            moredata = False
+            break
+        else:
+            print("Invalid input. Please enter 'Yes' or 'No'.")
+
+print ("Thank you for using this program.")
+    
+    
 
 
 
